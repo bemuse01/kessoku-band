7d561ce2ed09f2b3108fb16030f9444c5bcb18a7
bemuse01 <sequeland@gmail.com>
2023-05-04 19:47:24 +0900

    update

--- app.vue
+++ app.vue
@@ -6,4 +6,5 @@
 
 <style>
 @import url('./assets/css/main.css');
+@import url('./assets/css/anim.css');
 </style>
--- components/babylon/RenderBgRight.vue
+++ components/babylon/RenderBgRight.vue
@@ -3,6 +3,18 @@
         :class="classes.wrapper"
     >
 
+        <transition-group name="translate">
+            <div
+                v-for="bg in bgs"
+                :key="bg.key"
+                v-show="bg.show"
+                :class="classes.bg"
+                :style="bg.style"
+            >
+            </div>
+        </transition-group>
+
+
     </div>
 </template>
 
@@ -20,14 +32,33 @@ const {getIdx} = storeToRefs(store)
 
 // class
 const classes = reactive({
-    wrapper: 'absolute left-0 w-full h-full bg-white',
+    wrapper: 'absolute left-0 w-full h-full bg-white overflow-hidden',
+    bg: 'w-full h-full absolute'
 })
 
 
 // bg color
+const bgs = ref(Array.from({length: 2}, (_, key) => ({
+    key,
+    show: key === 0 ? true : false,
+    style: {
+        background: musics[getIdx.value].color
+    }
+})))
 const delay = 500
+const changeBg = () => {
+    bgs.value.forEach(bg => {
+        if(bg.show === true){
+            bg.show = false
+        }else{
+            bg.show = true
+            bg.style.background = musics[getIdx.value].color
+        }
+    })
+}
 watch(getIdx, debounce(function(cur, pre){
-    console.log(cur)
+    // console.log(cur)
+    changeBg()
 }, delay))
 </script>
 
