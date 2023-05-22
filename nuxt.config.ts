eaa9ba94e352b614445effb3b35652c5edc519e8
bemuse01 <sequeland@gmail.com>
2023-05-23 06:54:39 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default {
     app: {
         head: {
           charset: 'utf-8',
-          viewport: 'width=device-width, initial-scale=1',
+          viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0',
           title: 'kessoku band',
           meta: [
             { name: 'description', content: 'kessoku band player' }
